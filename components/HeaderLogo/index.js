2beed54c850a6168ee85d0c8ab10400714895d14
nmarquezdrubu <nmarquez@uade.edu.ar>
2020-05-24 23:20:01 -0300

    fix logo

--- components/HeaderLogo/index.js
+++ components/HeaderLogo/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Image, StyleSheet,View, Dimensions } from 'react-native';
+const win = Dimensions.get('window');
 
 export default class HeaderLogo extends Component {
   render() {
     return (
+      <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
       <Image
-        source={require('../../assets/logo-transparente.png')}
-        style={{ width: 200, height: 50 }}
+        source={require('../../assets/logo-blanco.jpeg')}
+        style={{width:200, height:50}}
+        resizeMode={'stretch'}  
       />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: win.width,
+    height: win.height,
+}
+});
+
